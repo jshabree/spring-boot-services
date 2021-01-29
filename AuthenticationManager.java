c7de2b6540bd7a210c8a3ffeb9dac32168d19d46
Juveria Shabreen <47865880+jshabree@users.noreply.github.com>
2021-01-29 09:26:15 -0500

    Delete AuthenticationManager.java

--- AuthenticationManager.java
+++
@@ -1,9 +0,0 @@
-public interface AuthenticationManager {
-
-    // returns true, if input is valid
-    // throws an exception if its not
-    // returns null if it can't decide 
-
-    Authentication authenticate(Authentication authentication )
-    throws AuthenticationException;
-}
