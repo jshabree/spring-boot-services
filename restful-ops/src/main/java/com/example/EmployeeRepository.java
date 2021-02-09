3ca333bbd665f555b2118c88704dc99157e0ebbd
Juveria Shabreen <47865880+jshabree@users.noreply.github.com>
2021-02-09 09:58:57 -0500

    RESTful API with hypertext

---
+++ restful-ops/src/main/java/com/example/EmployeeRepository.java
@@ -0,0 +1,7 @@
+package com.example;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    
+}
---
+++ restful-ops/src/main/java/com/example/restfulops/RestfulOpsApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfulops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulOpsApplication.class, args);
+	}
+
+}
